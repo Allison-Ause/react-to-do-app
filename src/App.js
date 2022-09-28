870262f602dfea94376c23c69b661a361d25602f
Allison Ause <allison.ause@gmail.com>
2022-09-28 14:33:29 -0700

    set up initial routes for components and base redirects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { Redirect, Route } from 'react-router-dom';
 import './App.css';
+import Auth from './components/Auth/Auth';
+import Items from './components/Items/Items';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Route path="/auth/:type" component={Auth} />
+      <Route path="/items" component={Items} />
+      <Route path="*">
+        <Redirect to="/auth/sign-up" />
+      </Route>
+      <Redirect to="/auth/sign-up" />
+    </div>
+  );
 }
 
 export default App;
