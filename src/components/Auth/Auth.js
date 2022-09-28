2bc874b385eafb5d271aaf56f091ca1aae09074c
Allison Ause <allison.ause@gmail.com>
2022-09-28 15:31:15 -0700

    added email and password submission for new user creation or user sign up including handleSubmit function

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,8 +1,36 @@
+import { useContext, useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { UserContext } from '../../context/userContext';
+import { authUser } from '../../services/auth';
+
 export default function Auth() {
+  const { user, setUser } = useContext(UserContext);
+  const { type } = useParams();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async () => {
+    const userRes = await authUser(email, password, type);
+    setUser(userRes);
+    setEmail('');
+    setPassword('');
+  };
+
+  if (user) {
+    return <Redirect to="/items" />;
+  }
+
   return (
     <div>
-      Auth Page
-      {/* TODO: Build sign up / sign in Form w/email, password and type */}
+      <label>
+        Email:
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
